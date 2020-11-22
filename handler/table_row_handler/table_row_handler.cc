07c11ddcce6e5df72920790f930453f2e5a789ce
Red_Hairy_Mouse <56833537+cjw-bot@users.noreply.github.com>
2020-11-22 12:02:35 +0800

    Create table_row_handler.cc

---
+++ handler/table_row_handler/table_row_handler.cc
@@ -0,0 +1,98 @@
+#include "table_row_handler.h"
+
+TableRowHandler::TableRowHandler(const string &file_path, TableMetaHandler *&table_meta_handler)
+    : table_name_(table_meta_handler->get_table_name()),
+      line_size_(table_meta_handler->get_line_size()),
+      off_set_of_each_column_(table_meta_handler->get_off_set_of_each_column()),
+      type_of_each_column_(table_meta_handler->get_type_of_each_column()),
+      table_meta_(table_meta_handler->get_table_meta())
+
+{
+    file_.open(file_path, 1, 1, 0, 1);
+}
+
+template <class T>
+T *TableRowHandler::read(uint64_t &off_set, uint64_t &line_number, list<int> &wanted_columns)
+{
+    void *buffer_pointer = file_.read_lines_into_buffer(off_set * line_size_, line_size_, line_number);
+    T *rows = new T;
+
+    // 解析buffer
+    for (int i = 0; i < line_number; i++)
+    {
+        // 存储一行数据
+        json row;
+
+        // 读取想要的行
+        for (auto it = wanted_columns.begin(); it != wanted_columns.end(); it++)
+        {
+            // 变量的初始读取地址
+            void *initial_address = buffer_pointer + off_set_of_each_column_[*it];
+
+            // 传入变量ID，映射为模板元变量，转化指针类型读取后push_back到row中
+            // 具体实现参考buffer_reader.h
+            read_buffer<10>(type_of_each_column_[*it], initial_address, row);
+        }
+
+        rows->push_back(row);
+    }
+    return rows;
+}
+
+template <class T>
+status_code TableRowHandler::write(const T &rows_information)
+{
+    // 行数目
+    int rows_number = rows_information.size();
+
+    // 需要写入的数据所占的内存
+    void *buffer = new char[line_size_ * rows_number];
+    if (!buffer)
+    {
+        return error_code::ERROR_MEMORY_ALLOCATION_FAIL;
+    }
+
+    // 遍历每一行
+    for (int i = 0; i < rows_number; i++)
+    {
+        const json &row = rows_information[i];
+
+        // 列序号，与数据类型一一对应
+        // 从1开始
+        int column_order = 1;
+
+        // 遍历每一列
+        // for (auto it = row.begin(); it != row.end(); it++, column_order++)
+        // {
+        //     // 计算从buffer写入该变量的初始地址
+        //     void *initial_address = buffer + line_size_ * i + off_set_of_each_column_[column_order];
+
+        //     const nlohmann::basic_json<>& temp = *it;
+
+        //     // 写入buffer
+        //     write_buffer(type_of_each_column_[column_order], initial_address, temp);
+        // }
+
+        for (int j = 0; j < row.size(); j++)
+        {
+            // 计算从buffer写入该变量的初始地址
+            void *initial_address = buffer + line_size_ * i + off_set_of_each_column_[column_order];
+
+            // 写入buffer
+            write_buffer(type_of_each_column_[column_order], initial_address, row, j);
+        }
+    }
+
+    status_code code = file_.append(buffer);
+    delete[](char *) buffer;
+
+    return code;
+}
+
+// 特化
+
+template json *TableRowHandler::read(uint64_t &off_set, uint64_t &line_number, list<int> &wanted_columns);
+template vector<json> *TableRowHandler::read(uint64_t &off_set, uint64_t &line_number, list<int> &wanted_columns);
+
+template status_code TableRowHandler::write(const vector<json> &rows_information);
+template status_code TableRowHandler::write(const json &rows_information);
