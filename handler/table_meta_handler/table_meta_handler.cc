2d56092d63fa040a948e00cdf1a210af8d83dd6c
Red_Hairy_Mouse <56833537+cjw-bot@users.noreply.github.com>
2020-11-22 12:02:19 +0800

    update table_meta_handler

--- handler/table_meta_handler/table_meta_handler.cc
+++ handler/table_meta_handler/table_meta_handler.cc
@@ -2,7 +2,7 @@
 
 const string TableMetaHandler::get_table_name()
 {
-    return table_meta_information_["table_name"];
+    return table_meta_["table_name"];
 }
 
 const string &TableMetaHandler::get_table_meta_path()
@@ -10,56 +10,212 @@ const string &TableMetaHandler::get_table_meta_path()
     return table_meta_path_;
 }
 
-const json &TableMetaHandler::get_table_meta_information()
+const json &TableMetaHandler::get_table_meta()
 {
-    return table_meta_information_;
+    return table_meta_;
 }
 
+// TableMetaHandler::TableMetaHandler()
+// {
+//     raise_exception(error_code::SUCCESS);
+// }
+
+// 通过load读取对应路径下元数据
 TableMetaHandler::TableMetaHandler(const string &table_meta_path)
+:table_meta_path_(table_meta_path)
 {
-    // TODO：路径检查：非空、特殊字符....
-    table_meta_path_ = table_meta_path;
-    load(); // 通过load读取对应路径下元数据
+    load(); 
 }
 
+// 直接传入元数据
 TableMetaHandler::TableMetaHandler(const string &table_meta_path, json &table_meta)
-{
-    // TODO：路径检查：非空、特殊字符....
-    table_meta_path_ = table_meta_path;
-    table_meta_information_ = table_meta; // 直接传入元数据
-}
+    :table_meta_(table_meta),
+    table_meta_path_(table_meta_path)
+{}
 
-void TableMetaHandler::save(const string &table_meta_path)
+status_code TableMetaHandler::save(const string &table_meta_path)
 {
     DataFileHandler file;
     file.open(table_meta_path, 1, 0, 1, 0);
-    file.append(table_meta_information_.dump());
+    return file.append(table_meta_.dump());
 }
 
-void TableMetaHandler::save()
+status_code TableMetaHandler::save()
 {
-    // 在构造函数加入路径合法性检查后，就可以删除以下判断了
-    // if (table_meta_information_["table_name"].empty() || table_meta_path_.empty() || table_meta_information_.empty())
-    //     throw "TableMetaHandler::save()：未初始化！";
-
     DataFileHandler file;
     file.open(table_meta_path_, 1, 0, 1, 0);
-
-    file.append(table_meta_information_.dump());
+    return file.append(table_meta_.dump()); // 返回错误码
 }
 
-void TableMetaHandler::load()
+status_code TableMetaHandler::load()
 {
-    // if (!table_meta_information_.empty())
-    //     throw "TableMetaHandler::load()：不能重复读取";
-
     DataFileHandler file;
     file.open(table_meta_path_, 0, 0, 0, 1);
 
-    pair<string, status_code> p = file.read_all();
+    respond<string> p = file.read_all();
 
     if (!p.first.empty())
-        table_meta_information_ = json::parse(p.first);
+    {
+        table_meta_.parse(p.first);
+        return error_code::SUCCESS;
+    }
     else
-        error_code::check_error(p.second);
+    {
+        return p.second;
+    } // 返回错误码
+}
+
+uint16_t TableMetaHandler::get_line_size()
+{
+    // 若已有缓存，直接返回
+    if (line_size_)
+        return line_size_;
+
+    set_column_info_and_line_size();
+    return line_size_;
+}
+
+vector<uint16_t> &TableMetaHandler::get_off_set_of_each_column()
+{
+    // 若已有缓存，直接返回
+    if (!off_set_of_each_column_.empty())
+        return off_set_of_each_column_;
+
+    set_column_info_and_line_size();
+    return off_set_of_each_column_;
+}
+
+vector<int8_t> &TableMetaHandler::get_type_of_each_column()
+{
+    if (!type_of_each_column_.empty())
+        return type_of_each_column_;
+
+    set_column_info_and_line_size();
+    return type_of_each_column_;
+}
+
+list<int> TableMetaHandler::get_column_orders_by_names(const list<string> &column_names)
+{
+    if (type_of_each_column_.empty())
+        set_column_info_and_line_size();
+
+    list<int> output;
+
+    for (auto it = column_names.begin(); it != column_names.end(); it++)
+    {
+        output.push_back(map_of_column_name_to_column_order_[*it]);
+    }
+    output.sort();
+
+    return output;
+}
+
+status_code TableMetaHandler::set_column_info_and_line_size()
+{
+    // 若已有缓存，直接返回
+    if (!off_set_of_each_column_.empty())
+        return error_code::SUCCESS;
+
+    json &primary_keys = table_meta_["primary_key"];
+    json &other_keys = table_meta_["other_keys"];
+
+    // 一共有这么多列（key）
+    uint8_t key_number = primary_keys.size() + other_keys.size();
+
+    // 新建一个数组用于存储每一个column（key）在row中的偏移位置
+    // 首位置存column数量
+    off_set_of_each_column_.resize(key_number + 1);
+    off_set_of_each_column_[0] = key_number;
+    off_set_of_each_column_[1] = 0;
+
+    // 新建数组存储column中key的类型
+    type_of_each_column_.resize(key_number + 1);
+    type_of_each_column_[0] = key_number;
+
+    if (!map_of_column_name_to_column_order_.empty())
+    {
+        map_of_column_name_to_column_order_.clear();
+    }
+
+    // 变量长度
+    int size_of_var = 0;
+
+    // 行长度
+    line_size_ = 0;
+
+    // 从off_set_of_each_column_第二个元素开始赋值
+    int i = 2;
+
+    /* 类型与对应数字翻译表：
+    1:int_8_t
+    2:int_16_t
+    3:int_32_t
+    4:int_64_t
+    5:uint_8_t
+    6:uint_16_t
+    7:uint_32_t
+    8:uint_64_t
+    9:float
+    10:double
+    -123:char（负数表示char，数值表示char的长度）（其实char的长度也可以通过前后偏移相减得到）*/
+    for (auto it = primary_keys.begin(); it != primary_keys.end(); it++, i++)
+    {
+        // 寻找key_type
+        auto type_it = it->find("type");
+        if (type_it == it->end())
+        {
+            return error_code::ERROR_KEY_TYPE_NOT_FOUND_IN_TABLE_META;
+        }
+
+        // key的数据种类
+        string key_type = *type_it;
+        if (key_type == "int")
+        {
+            // 字节数
+            size_of_var = (*it)["length"];
+            size_of_var = size_of_var / 8;
+
+            // 转为正数（由于json中存储时用负号表示signed int）
+            if (size_of_var < 0)
+            {
+                size_of_var = -size_of_var;
+                type_of_each_column_[i - 1] = size_of_var;
+            }
+            else // 正号表示unsigned
+            {
+                type_of_each_column_[i - 1] = size_of_var + 4;
+            }
+        }
+        else if (key_type == "char")
+        {
+            size_of_var = (*it)["length"];
+
+            // 负号表示char，数值表示长度
+            type_of_each_column_[i - 1] = -size_of_var;
+        }
+        else if (key_type == "float")
+        {
+            size_of_var = sizeof(float);
+            type_of_each_column_[i - 1] = 9;
+        }
+        else if (key_type == "double")
+        {
+            size_of_var = sizeof(double);
+            type_of_each_column_[i - 1] = 10;
+        }
+        else
+        {
+            return error_code::ERROR_KEY_TYPE_NOT_FOUND_IN_TABLE_META;
+        }
+
+        // 设置从列名到列序号的映射
+        map_of_column_name_to_column_order_[(*it)["name"]] = type_of_each_column_[i - 1];
+
+        // 加上当前key的类型对应的size
+        line_size_ += size_of_var;
+
+        // 第i个元素的地址偏移就等于前i-1个元素的size之和
+        off_set_of_each_column_[i] = size_of_var;
+    }
+    return error_code::SUCCESS;
 }
--- handler/table_meta_handler/table_meta_handler.h
+++ handler/table_meta_handler/table_meta_handler.h
@@ -4,12 +4,8 @@
 #include <stdexcept>
 #include <string>
 #include <fstream>
-
-namespace table_meta_handler_status_code
-{
-    // TODO
-
-} // namespace table_meta_handler_status_code
+#include <map>
+#include<list>
 
 // json解析库
 #include "lib/Json/single_include/nlohmann/json.hpp"
@@ -21,7 +17,6 @@ using namespace std;
 
 typedef int8_t status_code;
 
-// for convenience
 using json = nlohmann::json;
 
 // 处理表头的类：
@@ -29,23 +24,58 @@ using json = nlohmann::json;
 // 对表头的一切处理只能通过该类来完成
 //
 // 功能:
-// 通过该类的实例对表头的数据进行修改与存储与删除等操作
+// 通过且只能通过该类的实例对表头的数据进行修改与存储与删除等操作
 // 每次修改元数据，直到save才会将修改同步到磁盘，相当于一次commit
 //
 // 注意：
-// 对于每一个实例，你可以一直保留着，直到你不需要再使用到元数据
-// 需要注意的是，由于其他类使用引用读取表元数据，所以你不能释放实例直到你决定不再读取表头数据
+// 由于其他类的table_meta均是对此类table_meta的引用
+// 优点是该类对table_meta的所有更改会自适应到其他引用处
+// 缺点是对于每一个实例，你需要一直保留着，直到你不再需要修改或读取元数据
 class TableMetaHandler
 {
 private:
+
+    // 表元数据
+    json table_meta_;
+
     // 表元数据的存储路径
     string table_meta_path_;
 
-    // 表元数据
-    json table_meta_information_;
+    // 一行在磁盘中所占size
+    // 当首次调用时会进行计算并赋值
+    // 后续调用时会直接读取
+    uint16_t line_size_ = 0;
+
+    // 记录每一列对应的地址偏移，方便读写时定位，首元素存储长度。
+    // 当首次调用时会进行计算并赋值
+    // 后续调用时会直接读取
+    vector<uint16_t> off_set_of_each_column_;
+
+    // 用数组来存储每种类，首元素存储长度。
+    // 当首次调用时会进行计算并赋值
+    // 后续调用时会直接读取
+    /*
+    1:int_8_t
+    2:int_16_t
+    3:int_32_t
+    4:int_64_t
+    5:uint_8_t
+    6:uint_16_t
+    7:uint_32_t
+    8:uint_64_t
+    9:float
+    10:double
+    -123:char（负数表示char，数值表示char的长度）（其实char的长度也可以通过前后偏移相减得到）*/
+    vector<int8_t> type_of_each_column_;
+
+    // 存储每一列的列名与对应的序号
+    map<string, int> map_of_column_name_to_column_order_;
+
+    // 对off_set、type与linesize三个关于行列的成员变量进行计算并赋值
+    status_code set_column_info_and_line_size();
 
     // 通过类成员中的表头路径读取表元数据
-    void load();
+    status_code load();
 
 public:
     // 获取表名
@@ -56,11 +86,9 @@ public:
     const string &get_table_meta_path();
 
     // 获取表元数据，只读
-    const json &get_table_meta_information();
+    const json &get_table_meta();
 
-    // 通过以下两个函数初始化的方式不安全，已被弃用
-    // TableMetaHandler() = default;
-    // void set_table_meta_path(const string &table_meta_path);
+    // TableMetaHandler() = delete;
 
     // 传入表头路径，通过load函数读取表头数据
     TableMetaHandler(const string &table_meta_path);
@@ -69,11 +97,23 @@ public:
     TableMetaHandler(const string &table_meta_path, json &table_meta);
 
     // 使用传入的路径保存表头文件（另存为）
-    void save(const string &table_meta_path);
+    status_code save(const string &table_meta_path);
 
     // 使用类成员中的表头路径保存表头文件（即替换原文件内容）
-    void save();
+    status_code save();
 
-    // TODO：修改表元数据的函数，比如重命名，改属性之类的
+    // 获取一行的长度
+    // 当首次调用时会进行计算并赋值
+    // 后续调用时会直接读取
+    uint16_t get_line_size();
+
+    // 获取每行读取时的off_set、type
+    // 当首次调用时会进行计算并赋值
+    // 后续调用时会直接读取
+    vector<uint16_t> &get_off_set_of_each_column();
+    vector<int8_t> &get_type_of_each_column();
+
+    // 传入列名，获取对应的列序号并从小到大排序返回
+    list<int> get_column_orders_by_names(const list<string>&column_names);
 };
 #endif // MOUSEDB_HANDLER_TABLE_META_HANDLER_H_
