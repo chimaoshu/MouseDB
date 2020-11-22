bb354560d748cf2083fc78c6022472eba90e82a7
Red_Hairy_Mouse <56833537+cjw-bot@users.noreply.github.com>
2020-11-22 12:02:01 +0800

    update table_handler

--- handler/table_handler/table_handler.cc
+++ handler/table_handler/table_handler.cc
@@ -1,89 +1,60 @@
 #include "table_handler.h"
 
-#include <string>
-#include <iostream>
-
-// 删除文件
-#include <cstdio>
-
-#include "handler/data_file_handler/data_file_handler.h"
-
 TableHandler::TableHandler(const string database_name, const json &database_meta)
+    : database_meta_(database_meta), database_name_(database_name)
 {
-    database_name_ = database_name;
-    database_meta_ = database_meta;
+    string table_dir, table_name;
+    for (auto it = database_meta[database_name].begin(); it != database_meta[database_name].end(); it++)
+    {
+        table_name = it.key();
+        table_dir = it.value();
+
+        string table_meta_path = table_dir + table_name + ".json";
+        map_of_name_and_table_meta_handler_[table_name] = new TableMetaHandler(table_meta_path);
+    }
 }
 
-respond<string> TableHandler::get_table_dir(const string &table_name)
+const string &TableHandler::get_database_name() { return database_name_; }
+
+string TableHandler::get_table_dir(const string &table_name)
 {
     // 通过JSON获取
     auto it = database_meta_[database_name_].find(table_name);
     if (it != database_meta_[database_name_].end())
-        return respond<string>(it.key(),error_code::SUCCESS);
+        return it.key();
     else
-        return respond<string>("", error_code::ERROR_TABLE_INFO_NOT_FOUND);
+        return "";
 }
 
-status_code TableHandler::create(const string &table_directory, const string &table_name, const list<column *> &columns, DatabaseMetaHandler &db_mete_handler)
+status_code TableHandler::create(const string &table_directory, const string &table_name, json &table_meta, DatabaseMetaHandler &db_mete_handler)
 {
     // TODO：检查路径合法性（应以'/'结尾，且不包含特殊字符）
     // check(table_directory);
-
-    // 存入路径与表名
-    tables_in_database_[table_name]["storage_path"] = table_directory;
-    tables_in_database_[table_name]["table_name"] = table_name;
-
-    // 遍历每一个column，转化为JSON存入
-    for (list<column *>::const_iterator i = columns.begin(); i != columns.end(); i++)
-    {
-        column *each_column = *i;
-
-        // 代表某列数据的JsonObject
-        json each_column_in_json;
-
-        each_column_in_json["name"] = each_column->key_name;
-        each_column_in_json["type"] = each_column->type;
-        each_column_in_json["not_null"] = each_column->not_null;
-
-        if (each_column->is_primary)
-            tables_in_database_[table_name]["primary_key"] = each_column_in_json;
-        else
-            tables_in_database_[table_name]["other_keys"].push_back(each_column_in_json);
-    }
-
-    // 保存表头JSON到磁盘
-    string table_meta_path = table_directory + table_name + ".json";
-    TableMetaHandler table_meta_ = TableMetaHandler(table_meta_path, tables_in_database_);
-    table_meta_.save();
 
     // 创建表文件
     string table_path = table_directory + table_name + ".mdb";
     DataFileHandler table;
     table.open(table_path, 1, 0, 0, 0);
 
-    // 在数据库元数据JSON中增加该表信息
-    status_code err = db_mete_handler.add_table_in_json(database_name_, table_name, table_directory);
-
-    // 保存到磁盘
+    // 在数据库元数据JSON中增加该表信息并保存到磁盘
+    db_mete_handler.add_table_in_json(database_name_, table_name, table_directory);
     db_mete_handler.save();
 
-    // 返回状态码
-    return err;
+    // 保存表头JSON到磁盘，并返回状态码
+    string table_meta_path = table_directory + table_name + ".json";
+    map_of_name_and_table_meta_handler_[table_name] = new TableMetaHandler(table_meta_path, table_meta);
+    return map_of_name_and_table_meta_handler_[table_name]->save();
 }
 
 status_code TableHandler::drop_table(const string &table_name, DatabaseMetaHandler &db_mete_handler)
 {
-    respond<string> table_dir = get_table_dir(table_name);
-
-    // 检查错误码
-    if (table_dir.second)
-        return table_dir.second;
+    string table_dir = get_table_dir(table_name);
 
     // 先删除二进制文件
-    remove((table_dir.first + table_name + ".mdb").c_str());
+    remove((table_dir + table_name + ".mdb").c_str());
 
     // 然后删除表头
-    remove((table_dir.first + table_name + ".json").c_str());
+    remove((table_dir + table_name + ".json").c_str());
 
     // 调用DatabaseMetaHandler来抹去JSON上该表数据
     status_code error = db_mete_handler.delete_table_in_json(database_name_, table_name);
@@ -91,19 +62,37 @@ status_code TableHandler::drop_table(const string &table_name, DatabaseMetaHandl
     // 保存到磁盘
     db_mete_handler.save();
 
+    // 删除tables_info中的表头数据
+    auto it = map_of_name_and_table_meta_handler_.find(table_name);
+    if (it != map_of_name_and_table_meta_handler_.end())
+        map_of_name_and_table_meta_handler_.erase(it);
+
     // 返回状态码
     return error;
 }
 
-vector<string> *TableHandler::get_all_table_names()
+list<string> TableHandler::get_all_table_names()
 {
-    vector<string> *all_table_names = new vector<string>;
+    list<string> all_table_name;
 
-    // 获取所有key
-    for (auto it = database_meta_[database_name_].begin(); it != database_meta_[database_name_].end(); it++)
+    // 遍历map_of_name_and_table_meta_handler的第一个元素获取keys
+    for (auto it = map_of_name_and_table_meta_handler_.begin(); it != map_of_name_and_table_meta_handler_.end(); it++)
     {
-        all_table_names->push_back(it.key());
+        all_table_name.push_back(it->first);
     }
+    return all_table_name;
+}
 
-    return all_table_names;
+const json &TableHandler::get_table_meta(const string &table_name)
+{
+    auto it = map_of_name_and_table_meta_handler_.find(table_name);
+    if (it != map_of_name_and_table_meta_handler_.end())
+        return it->second->get_table_meta();
+    else
+        return NULL;
+}
+
+TableMetaHandler *&TableHandler::get_table_meta_handler(const string &table_name)
+{
+    return map_of_name_and_table_meta_handler_[table_name];
 }
--- handler/table_handler/table_handler.h
+++ handler/table_handler/table_handler.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <list>
+#include <map>
 
 #include "lib/Json/single_include/nlohmann/json.hpp"
 
@@ -18,33 +19,29 @@
 using namespace std;
 using json = nlohmann::json;
 
-// 元数据中的一列的定义
-struct column
-{
-    string key_name;
-    string type;
-    bool is_primary;
-    bool not_null;
-};
-
 // 对某个数据库中表的处理，包括表的创建、打开、删除
 // TableHandler连接起了表元数据与数据库元数据库
 // 与TableMetaHandler不同的是，该类管理某个数据库中的所有表
-// 即一个实例对应一个被打开的数据库，用户进入一个数据库时，一个TableHandler将会被实例化
+// 即一个TableHandler实例将对应一个被打开的数据库
+// 用户进入一个数据库时，一个TableHandler将会被实例化
+// 该类不应该被释放，除非您确定以后不会再使用该数据库
+// 即使暂时不使用该数据库，也建议（在UserStatusHandler中？或者DBHandler中？）用一个MAP把数据库名与该实例缓存起来
 class TableHandler
 {
 private:
     // 数据库头JSON，存储了所有的数据库和对应的表及其路径
-    // 具体格式参考format/database_meta.jsonc
-    json database_meta_;
-
-    // 存储从表名到表头JSON对象的映射
-    // 每次打开一个表头JSON文件都存储一对
-    // JSON对象的具体格式参考format/table_meta.jsonc
-    json tables_in_database_;
+    // 来自对DatabaseMetaHandler成员的引用
+    // 格式参考format/database_meta.jsonc
+    const json &database_meta_;
 
     // 当前打开的数据库名（由于一个该类的实例对应一个被打开的数据库）
     string database_name_;
+
+    // 存储从表名到table_meta_handler的映射
+    // 每次创建TableHandler都会在此处缓存所有表对应的TableMetaHandler，方便读操作
+    // 内容包括：表名及其对应元数据
+    // 格式参考format/tables_info.jsonc
+    map<string, TableMetaHandler*> map_of_name_and_table_meta_handler_;
 
 public:
     // 构造函数，传入数据库元数据的JSON信息（databse_meta）
@@ -54,30 +51,34 @@ public:
     // 使引用了该JSON的实例的类成员也同步得到修改
     TableHandler(const string database_name_, const json &database_meta);
 
-    // 该构造函数只用于在检查不通过时，返回一个没用的实例
-    // 因此不允许在其他条件下调用
-    TableHandler();
+    // 获得数据库名
+    const string &get_database_name();
+
+    // 传入表名、返回对应的table_meta的引用
+    // 若不存在，返回NULL
+    const json &get_table_meta(const string &table_name);
+
+    // 返回对TableMetaHandler的引用
+    TableMetaHandler *&get_table_meta_handler(const string &table_name);
 
     // 传入一个表名、获取这个表名对应的表的存储路径
     // 若表不存在，返回空字符串
-    respond<string> get_table_dir(const string &table_name);
+    string get_table_dir(const string &table_name);
 
     // 创建一个表
     // table_directory：表的存储路径
     // table_name：表名
-    // columns：各列信息
-    status_code create(const string &table_directory, const string &table_name, const list<column *> &columns, DatabaseMetaHandler &db_mete_handler);
+    // table_meta：表头
+    // db_mete_handler：对数据库元数据进行操作
+    status_code create(const string &table_directory, const string &table_name, json &table_meta, DatabaseMetaHandler &db_mete_handler);
 
     // 传入表名与DatabaseMetaHandler
     // 删除一个表，所做的工作包括：
     // 在database_meta中抹除该表信息并保存、在磁盘上删除对应表路径下的JSON与MDB文件
     status_code drop_table(const string &table_name, DatabaseMetaHandler &db_mete_handler);
 
-    // 获取所有表的表名
-    // 返回一个vector<string>的指针，使用完成需要释放
-    vector<string> *get_all_table_names();
-
-    // open一个表的过程不在此处实现，由TableRow自己去操作
+    // 返回该数据库下所有表的表名
+    list<string> get_all_table_names();
 };
 
 #endif // MOUSEDB_HANDLER_TABLE_HANDLER_H_
