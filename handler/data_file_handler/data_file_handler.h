44fad78c0f8005942eadf778d06893240360080a
Red_Hairy_Mouse <56833537+cjw-bot@users.noreply.github.com>
2020-11-22 12:00:45 +0800

    Update data_file_handler.h

--- handler/data_file_handler/data_file_handler.h
+++ handler/data_file_handler/data_file_handler.h
@@ -8,8 +8,10 @@
 #include <fstream>
 
 #include "handler/exception_handler/exception_handler.h"
+#include "lib/Json/single_include/nlohmann/json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 // 文件处理类
 // 使用默认构造函数，因此需要在实例化后自行调用open打开文件，再进行后续操作。
@@ -29,15 +31,15 @@ private:
     // 打开的文件
     fstream file_;
 
-    // 路径下的文件是否存在
-    bool file_exist(const string &path);
-
 public:
     DataFileHandler() = default;
     ~DataFileHandler();
 
-    // 返回文件路径，可读不可写
-    const string &get_path();
+    // 路径下的文件是否存在
+    bool file_exist(const string &path);
+
+    // 返回文件路径
+    string get_path();
 
     // 给成员变量file赋值
     // 只要不可读，在文件不存在时，会创建文件
@@ -52,21 +54,26 @@ public:
     // 文件是否已经打开
     bool is_open();
 
+    // 关闭文件
+    void close() { file_.close(); }
+
     // 在文件末尾追加字符串
     // 以string形式返回文件下一行的内容
-    // 若已经是最后一行，则string为空
     // 同时get指针会向下移一行
+    // 若已经是最后一行，则string为空
     status_code append(const string &string_to_write);
 
-    // TODO：在文件末尾追加二进制数据
-    // void append();
+    // 在文件末尾追加二进制数据
+    status_code append(void* buffer);
 
     // 读取一行字符串，返回字符串和状态码
     // 在异常或者已读取到底的情况下，字符串为空
     respond<string> read_line();
 
-    // TODO：读取数据库一行内容
-    // read_line()
+    // 从initial_adress开始，以每行line_size的大小，读line_number行
+    // 把lines读进缓存中，返回指向缓存的指针
+    // 若创建内存失败，返回空指针
+    void *read_lines_into_buffer(const int &initial_adress, const int &line_size, const int &line_number);
 
     // 读取整个文件内容，返回字符串和状态码
     // 在异常或者已读取到底的情况下，字符串为空
